5ae3d94d2b2d6d83207c0401724b2cc866dcb909
Belarty <belkinartemiy.dev@gmail.com>
2022-07-13 17:53:58 +0300

    more changing config and adding env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  poweredByHeader: false,
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+    APP_ENV: process.env.REACT_APP_ENV,
+  },
 }
 
 module.exports = nextConfig
