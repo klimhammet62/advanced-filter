e58a6c143bbc502d177546f102770e607f1d84e5
Belarty <belkinartemiy.dev@gmail.com>
2022-07-22 15:17:46 +0300

    Updates

--- redux/reducers/counter/counterSlice.ts
+++ redux/reducers/counter/counterSlice.ts
@@ -102,13 +102,13 @@ export const selectCount = (state: AppState) => state.counter.obj;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd =
+/* export const incrementIfOdd =
 	(amount: number): AppThunk =>
 	(dispatch, getState) => {
 		const currentValue = selectCount(getState());
 		if (currentValue % 2 === 1) {
 			dispatch(incrementByAmount(amount));
 		}
-	};
+	}; */
 
 export default counterSlice.reducer;
